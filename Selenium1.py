f92db50893597d4a74f243d29cc03408f56255b5
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-09-24 13:56:01 +0200

    1

--- Selenium1.py
+++ Selenium1.py
@@ -7,23 +7,40 @@ from selenium.common.exceptions import NoSuchElementException
 def make_screenshot(driver):
     today = datetime.datetime.today()
     short_date = today.strftime('_stamp%H%M%S')
-    screen_name = 'screen' + short_date + '.png'
+    screen_name = 'C:\\Users\\vdi-student\\Desktop\\' + 'screen' + short_date + '.png'
     driver.get_screenshot_as_file(screen_name)
 
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get('https://www.saucedemo.com/')
 
-username_field = driver.find_element('id', 'user-name')
+try:
+    username_field = driver.find_element('id', 'user-name')
+except NoSuchElementException:
+    print('nie znaleziono pola "user-name" po ID. Szukam po nazwie ')
+    username_field = driver.find_element('name', 'user-name')
+    make_screenshot(driver)
+
+username_field.clear()
 username_field.send_keys('standard_user')
 
 #driver.find_element('id', 'user-name').send_keys('standard_user')
 
-password_field = driver.find_element(By.XPATH, '//*[@id="password"]')
+try:
+    password_field = driver.find_element(By.XPATH, '//*[@id="password"]')
+except NoSuchElementException:
+    print('Nie znaleziono pola z haslem')
+    driver.quit()
+    raise
+password_field.clear()
 password_field.send_keys('secret_sauce')
 
 login_button = driver.find_element('name', 'login-button')
-login_button.click()
+if not login_button.get_attribute('disable'):
+    login_button.click()
+else:
+    print('przycisk nieaktywny')
+
 time.sleep(2)
 
 make_screenshot(driver)
