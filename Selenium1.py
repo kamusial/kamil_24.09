141537e4a170ff8cb040dd8e473822d0733e1f1d
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-09-24 12:17:02 +0200

    1

---
+++ Selenium1.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+import time
+import datetime
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+
+
+webdriver.common.by.By.NAME
+By.NAME
