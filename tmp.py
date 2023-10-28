9f891022ce47f5ce827dcc02c2edbc9183d2042f
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-10-28 09:30:40 +0200

    1

---
+++ tmp.py
@@ -0,0 +1,13 @@
+number1 = float(input('Podaj liczbe 1:  '))
+number2 = float(input('Podaj liczbe 2:  '))
+
+try:
+    result = number1 / number2
+    print(f'WYnik dzielenia to: {result}')
+except ZeroDivisionError:
+    result = 0
+    print('Dzielenie sie nie powiodło, result = 0')
+
+print('dalsza częsć programu')
+
+
