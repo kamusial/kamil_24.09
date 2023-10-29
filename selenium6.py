a96eef5182c9bd6bbfed5d87ff740bf9f0a1fddc
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-10-29 10:47:29 +0100

    1

--- selenium6.py
+++ selenium6.py
@@ -4,7 +4,9 @@ from selenium3_OOP import LoginPage
 import time
 from Selenium1 import make_screenshot
 
-driver = webdriver.Chrome()
+options = webdriver.ChromeOptions()
+options.add_argument('--headless=new')
+driver = webdriver.Chrome(options=options)
 driver.maximize_window()
 driver.get('https://www.w3schools.com/')
 time.sleep(2)
@@ -14,8 +16,6 @@ time.sleep(2)
 menu = driver.find_element('id', 'navbtn_tutorials')
 HTMLtutorial = driver.find_element('xpath', '/html/body/div[2]/div[2]/div/nav[1]/div[1]/div/div[2]/div[1]/div[1]/a[2]')
 
-
-
 webdriver.ActionChains(driver).move_to_element(menu).click().move_to_element(HTMLtutorial).click().perform()
 time.sleep(2)
 
@@ -24,4 +24,19 @@ HTML_form_attributes.click()
 tryityourself = driver.find_element('xpath', '//*[@id="main"]/div[3]/a')
 tryityourself.click()
 print(driver.title)
-# przejść do drugiej karty
+time.sleep(5)
+
+currentWindowName = driver.current_window_handle
+windowsNames = driver.window_handles
+print(currentWindowName)
+print(windowsNames)
+
+driver.switch_to.window(windowsNames[1])
+print(driver.title)
+print(driver.current_url)
+
+# for okno in windowNames:
+#     if okno != currentWindowName:
+#         driver.switch_to.window(okno)
+# # przejść do drugiej karty
+
