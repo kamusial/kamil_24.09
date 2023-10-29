e562cf94895168514eeaeee887eb2c91f4e0554e
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-10-29 09:19:00 +0100

    1

--- selenium5.py
+++ selenium5.py
@@ -1,19 +1,24 @@
+import pytest
 from selenium import webdriver
 from selenium3_OOP import LoginPage
 import time
 from Selenium1 import make_screenshot
 
-def test_login_page():
+
+@pytest.mark.parametrize('username, url',
+                         [('standard_user', 'https://www.saucedemo.com/inventory.html'),
+                          ('lock_out_user', 'https://www.saucedemo.com/')])
+def test_login_page(username, url):
     driver = webdriver.Chrome()
     page = LoginPage(driver)
     page.open()
-    page.enter_username('standard_user')
+    page.enter_username(username)
     page.enter_password('secret_sauce')
     page.click_login()
     time.sleep(1)
 
     try:
-        assert driver.current_url == 'https://www.saucedemo.com/inventory.html', make_screenshot(driver)
+        assert driver.current_url == url, make_screenshot(driver)
     except AssertionError:
         print('Błąd, adres url się nie zgadza')
         raise
